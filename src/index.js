f7076e9a67e040dc24c0c75e97b297a95f9c70a2
Bud_Ton <d2023ujle@gmail.com>
2024-12-03 20:01:36 +0300

    Correction commit v1.1

--- src/index.js
+++ src/index.js
@@ -4,4 +4,8 @@ import "./js/app";
 
 import "./img/goblin.png";
 
+// import { interval } from "./js/app.js";
+
+// console.log(interval);
+
 // TODO: write your code in app.js
--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,42 @@
-import GoblinBurrow from "./goblinBurrow/goblinBurrow.js";
+import img from "../img/goblin.png";
 
-const goblin = new GoblinBurrow(document.querySelector(".game-field"));
-goblin.getRandomPosition();
+const lengthList = 16;
+const goblinBurrow = document.createElement("ul");
+goblinBurrow.classList = "game-field";
+let i = 0;
+
+while (i < lengthList) {
+  i++;
+  goblinBurrow.insertAdjacentHTML(
+    "beforeend",
+    `<li class="goblin-burrow" data-id="${i}"></li>`,
+  );
+}
+document.body.appendChild(goblinBurrow);
+
+export default function game() {
+  const icon = `<img src="${img}" class="img-goblin" alt="Catch!!!" id="img">`;
+
+  const fields = document.querySelectorAll(".goblin-burrow");
+  const arr = [];
+
+  function getRandom(max) {
+    let randomA = Math.floor(Math.random() * max);
+    while (randomA === arr[0]) {
+      randomA = Math.floor(Math.random() * max);
+    }
+    arr[0] = randomA;
+    return randomA;
+  }
+
+  const random = getRandom(fields.length);
+  fields.forEach((item) => {
+    const goblinsFace = item.querySelector("#img");
+
+    if (goblinsFace) {
+      goblinsFace.remove();
+    }
+  });
+  fields[random].insertAdjacentHTML("beforeend", icon);
+}
+export const interval = setInterval(() => game(), 1000);
